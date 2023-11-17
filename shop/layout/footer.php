8aaea795b04c798da5c9dfe31e00ec62775ccd01
SuzzenD <nhatnmpc06310@fpt.edu.vn>
2023-11-17 14:03:44 +0700

    dang nhap, dang ky, giỏ hàng

---
+++ shop/layout/footer.php
@@ -0,0 +1,51 @@
+<div class="footer">
+            <div class="footer-info">
+               <div class="container">
+                  <div class="row">
+                     <div class="col-md-3">
+                        <div class="footer-logo"><a href="#"><img src="images/image-removebg-preview (2).png" alt=""></a></div>
+                     </div>
+                     <div class="col-md-3 col-sm-6">
+                        <h4 class="title"><strong>Thông Tin</strong> Liên Hệ</h4>
+                        <p>56-58 Phan Bội Châu, Phường Bến Thành, Quận 1 , Ho Chi Minh City</p>
+                        <p>Liên lạc: 070 378 6624</p>
+                        <p>Email :fengsystem.official@gmail.com</p>
+                     </div>
+                     <div class="col-md-3 col-sm-6">
+                        <h4 class="title"><strong>Hỗ Trợ </strong>Hàng Hóa</h4>
+                        <ul class="support">
+                           <li><a href="#">Giao dịch</a></li>
+                           <li><a href="#">Phương thức thanh toán</a></li>
+                           <li><a href="#">Đặt hàng</a></li>
+                           <li><a href="#">Thông tin hàng</a></li>
+                        </ul>
+                     </div>
+                     <div class="col-md-3">
+                        <h4 class="title">Mã <strong>Khuyến mãi </strong></h4>
+                        <button> Sale Code </button>
+                        <form class="newsletter">
+							<input type="text" name="" placeholder="Nhập email của bạn....">
+							<input type="submit" value="Đăng ký" class="button">
+						</form>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <div class="copyright-info">
+               <div class="container">
+                  <div class="row">
+                     <div class="col-md-6">
+                        <!-- <p>Copyright © 2012. Designed by <a href="#">Michael Lee</a>. All rights reseved</p> -->
+                     </div>
+                     <div class="col-md-6">
+                        <ul class="social-icon">
+                           <li><a href="#" class="linkedin"></a></li>
+                           <li><a href="#" class="google-plus"></a></li>
+                           <li><a href="#" class="twitter"></a></li>
+                           <li><a href="#" class="facebook"></a></li>
+                        </ul>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
