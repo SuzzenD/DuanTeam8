052c05ecf2b1b415f4be3159a6e31d46912979ea
Bun194 <votanluon194@gmail.com>
2023-12-03 20:15:03 +0700

    [PHP] - load (contact)

---
+++ shop/contact.php
@@ -0,0 +1,198 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  <?php include "./layout/header.php"; ?>
+  </head>
+  <body>
+    <div class="wrapper">
+    <?php include "./layout/nav.php" ?>
+      <div class="clearfix"></div>
+      <div class="container_fullwidth">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12">
+              <h5 class="title contact-title">Thông tin liên hệ</h5>
+              <div class="clearfix"></div>
+              <div class="map">
+                <iframe
+                  width="600"
+                  height="350"
+                  src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Vietnam&amp;aq=&amp;sll=14.058324,108.277199&amp;sspn=21.827722,43.286133&amp;ie=UTF8&amp;hq=&amp;hnear=Vietnam&amp;ll=14.058324,108.277199&amp;spn=8.883583,21.643066&amp;t=m&amp;z=6&amp;output=embed"
+                >
+                </iframe>
+              </div>
+              <div class="clearfix"></div>
+              <div class="row">
+                <div class="col-md-4">
+                  <div class="contact-infoormation">
+                    <h5>Thông tin liên hệ</h5>
+                    <p>
+                      Chính vì vậy, chúng tôi đã tạo ra trang web mới với mong muốn đem lại trải nghiệm người dùng tốt nhất. Chúng tôi cam kết luôn cải thiện và phát triển để đáp ứng mọi nhu cầu của bạn. Hãy đồng hành cùng chúng tôi trên hành trình này!
+                    </p>
+                    <ul>
+                      <li>
+                        <span class="icon">
+                          <img src="images/message.png" alt="" />
+                        </span>
+                        <p>
+                          Email :fengsystem.official@gmail.com
+                          <br />
+                          fengsystem@gmail.com
+                        </p>
+                      </li>
+                      <li>
+                        <span class="icon">
+                          <img src="images/phone.png" alt="" />
+                        </span>
+                        <p>
+                          084. 93 668 2236
+                          <br />
+                          084. 93 668 6789
+                        </p>
+                      </li>
+                      <li>
+                        <span class="icon">
+                          <img src="images/address.png" alt="" />
+                        </span>
+                        <p>
+                          TP. HCM
+                          <br />
+                          TP. CT
+                        </p>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div class="col-md-8">
+                  <div class="ContactForm">
+                    <h5>Mẫu liên hệ</h5>
+                    <form>
+                      <div class="row">
+                        <div class="col-md-6">
+                          <label>
+                            Họ và tên
+                            <strong class="red"> * </strong>
+                          </label>
+                          <input class="inputfild" type="text" name="" />
+                        </div>
+                        <div class="col-md-6">
+                          <label>
+                            Email
+                            <strong class="red"> * </strong>
+                          </label>
+                          <input class="inputfild" type="email" name="" />
+                        </div>
+                      </div>
+<div class="row">
+                        <div class="col-md-12">
+                          <label>
+                            Nội dung liên hệ
+                            <strong class="red"> * </strong>
+                          </label>
+                          <textarea class="inputfild" rows="8" name="">
+                          </textarea>
+                        </div>
+                      </div>
+                      <button class="pull-right">Gửi</button>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="clearfix"></div>
+          <div class="our-brand">
+            <h3 class="title">
+              Thương hiệu
+            </h3>
+            <div class="control">
+              <a id="prev_brand" class="prev" href="#"> &lt; </a>
+              <a id="next_brand" class="next" href="#"> &gt; </a>
+            </div>
+            <ul id="braldLogo">
+              <li>
+                <ul class="brand_item">
+                  <li>
+                    <a href="#">
+                      <div class="brand-logo">
+                        <img src="images/envato.png" alt="" />
+                      </div>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <div class="brand-logo">
+                        <img src="images/themeforest.png" alt="" />
+                      </div>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <div class="brand-logo">
+                        <img src="images/photodune.png" alt="" />
+                      </div>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <div class="brand-logo">
+                        <img src="images/activeden.png" alt="" />
+                      </div>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <div class="brand-logo">
+                        <img src="images/envato.png" alt="" />
+                      </div>
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <ul class="brand_item">
+                  <li>
+                    <a href="#">
+                      <div class="brand-logo">
+                        <img src="images/envato.png" alt="" />
+                      </div>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <div class="brand-logo">
+                        <img src="images/themeforest.png" alt="" />
+                      </div>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <div class="brand-logo">
+                        <img src="images/photodune.png" alt="" />
+                      </div>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <div class="brand-logo">
+                        <img src="images/activeden.png" alt="" />
+                      </div>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <div class="brand-logo">
+                        <img src="images/envato.png" alt="" />
+                      </div>
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+    <?php include "./layout/footer.php" ?>
+  </body>
+</html>
