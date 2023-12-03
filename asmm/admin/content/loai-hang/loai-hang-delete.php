f307a50d23e10f426e9cde23313dda160a187e6f
Bun194 <votanluon194@gmail.com>
2023-12-03 20:23:56 +0700

    [PHP][ADMIN] - update (loai-hang-delete)

--- asmm/admin/content/loai-hang/loai-hang-delete.php
+++ asmm/admin/content/loai-hang/loai-hang-delete.php
@@ -1,5 +1,13 @@
 <?php
-    extract($_REQUEST);
-    loai_hang_delete($ma_loai);
-    header('location: index.php?act=loaihang');
+    require_once ('dao/loai-hang.php'); // Thay thế bằng đường dẫn thực tế của tệp chứa các hàm xử lý loại hàng
+    // require_once(__DIR__ . 'dao/loai-hang.php');
+
+    if(isset($_REQUEST['ma_loai'])){
+        $ma_loai = $_REQUEST['ma_loai'];
+        loai_hang_soft_delete($ma_loai);
+        header('location: index.php?act=loaihang');
+    } else {
+        // Xử lý khi không có mã loại hàng được truyền vào
+        echo "Không có mã loại hàng được cung cấp.";
+    }
 ?>
